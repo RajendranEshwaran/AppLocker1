ead7b8cdd51a86e5593d4ce90127d0e724bfba36
RajendranEshwaran <rajendranmca2010@gmail.com>
2019-08-01 22:14:51 -0400

    App locker:- Modules options grid design and click listener implemented

--- app/src/main/java/com/rajendraneshwaran/applocker/MainActivity.java
+++ app/src/main/java/com/rajendraneshwaran/applocker/MainActivity.java
@@ -1,13 +1,42 @@
 package com.rajendraneshwaran.applocker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.GridLayout;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public GridLayout mainGridLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainGridLayout = (GridLayout)findViewById(R.id.mainGrid);
+        setSingleGridLayout(mainGridLayout);
+    }
+
+    private void setSingleGridLayout(GridLayout mGridLayout)
+    {
+        for(int i = 0; i < mGridLayout.getChildCount();i++)
+        {
+            CardView cardView = (CardView)mGridLayout.getChildAt(i);
+            final int finalIndex = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    switch (finalIndex) {
+                        case 0:
+                        Intent intent = new Intent(MainActivity.this, AppsLock.class);
+                        startActivity(intent);
+                        break;
+                    }
+                }
+            });
+        }
     }
 }
