eefe4dd048f6a61f9507d6d4089b1add99e0d6bc
RajendranEshwaran <rajendranmca2010@gmail.com>
2019-08-08 17:42:42 -0400

    App locker:- Pin create view and pin save process completed. pin saved in shared preference

--- app/src/main/java/com/rajendraneshwaran/applocker/MainActivity.java
+++ app/src/main/java/com/rajendraneshwaran/applocker/MainActivity.java
@@ -1,6 +1,9 @@
 package com.rajendraneshwaran.applocker;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     private IndicatorDots mIndicatorDots;
     public static final String TAG ="Pinview";
 
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String accessPin = "AccessPin";
+    SharedPreferences sharedpreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,17 +43,34 @@ public class MainActivity extends AppCompatActivity {
         mPinLockView.setPinLockListener(mPinLockListener);
         //mPinLockView.setCustomKeySet(new int[]{2, 3, 1, 5, 9, 6, 7, 0, 8, 4});
         //mPinLockView.enableLayoutShuffling();
+        mPinLockView.getDeleteButtonPressedColor();
 
         mPinLockView.setPinLength(4);
         mPinLockView.setTextColor(ContextCompat.getColor(this, R.color.white));
 
         mIndicatorDots.setIndicatorType(IndicatorDots.IndicatorType.FILL_WITH_ANIMATION);
+
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        String pinValue = sharedpreferences.getString("AccessPin", "");
+        if(pinValue.toString().isEmpty())
+        {
+            // Create Pin
+        }
+        if(!pinValue.toString().isEmpty())
+        {
+            // Already Pin Created
+        }
+
     }
 
     private PinLockListener mPinLockListener = new PinLockListener() {
         @Override
         public void onComplete(String pin) {
             Log.d(TAG, "Pin complete: " + pin);
+
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putString(accessPin, pin);
+            editor.commit();
 
             Intent intent = new Intent(MainActivity.this, PinView.class);
             startActivity(intent);
